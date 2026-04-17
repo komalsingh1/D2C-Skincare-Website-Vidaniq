03159eda5512da4bc98b1c9d873bd113fab1f84e
Komal Singh <komal17354@gmail.com>
2026-04-17 14:35:13 +0530

    Update next.config.mjs with correct configuration including basePath and trailing slash.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/D2C-Skincare-Website-Vidaniq",
+  output: 'export',
+  basePath: '/D2C-Skincare-Website-Vidaniq',
   trailingSlash: true,
   images: {
     unoptimized: true,
